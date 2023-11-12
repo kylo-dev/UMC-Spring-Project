6c89b4f54bb0d9cf3c48677228d7ab16fccc9789
hyungyeom <rlagusrua3687@gmail.com>
2023-11-12 22:57:58 +0900

    feat : Entity 생성

---
+++ src/main/java/umc/spring/domain/Mission.java
@@ -0,0 +1,28 @@
+package umc.spring.domain;
+
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer reward;
+
+    private LocalDateTime deadline;
+
+    private String missionSpec;
+}
